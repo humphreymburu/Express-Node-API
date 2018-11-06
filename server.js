15f5b81067394e3ba4b1cc6b6c0a423a614ce1c8
Humphrey <humphrey.asante@gmail.com>
2018-11-06 10:27:18 +0300

    start express server

---
+++ server.js
@@ -0,0 +1,15 @@
+//import express from 'express'
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => res.json({
+    ok: true
+}))
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+})
